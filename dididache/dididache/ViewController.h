a2a167e90c53fe26d4aa545dc443cab37bc56b6d
work <work@workdeMacBook-Pro.local>
2016-02-12 12:23:26 +0800

    初始化项目

---
+++ dididache/dididache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dididache
+//
+//  Created by work on 16/2/12.
+//  Copyright (c) 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ dididache/dididache/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  dididache
+//
+//  Created by work on 16/2/12.
+//  Copyright (c) 2016年 itcast. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+    
+    NSMutableString *str = [[NSMutableString alloc] init];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
